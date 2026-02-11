eaa6a5503243629fb59ce2d8a12a02b7fc8fc50b
Saksham Shakya <Sakshamshakya231@gmail.com>
2026-02-11 23:48:17 +0530

    fix deployment 1

---
+++ api/_middleware.js
@@ -0,0 +1,15 @@
+// Middleware for CORS
+export default function middleware(req, res, next) {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  // Handle preflight
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  
+  next();
+}
